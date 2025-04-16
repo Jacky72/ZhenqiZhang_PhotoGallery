830054da92c38e647b365ddec754990607a74a6c
Jacky72 <initialstarry2002@gmail.com>
2025-04-16 21:24:56 +0800

    Fix the small problem that the animation doesnt show up for when we switch the tag. Problem: The animation of the first 3 photos will be skipped if the tag you are on origionally have 3 pictures

--- zhenqizhang_photogallery/src/app/components/PictureSection.jsx
+++ zhenqizhang_photogallery/src/app/components/PictureSection.jsx
@@ -3,7 +3,7 @@ import React, { useState, useRef  } from 'react'
 import Tags from './Tags';
 import PhotoCards from './PhotoCards';
 import pictureData from './PictureData';
-import { motion, useInView } from "framer-motion";
+import { motion, useInView, AnimatePresence } from "framer-motion";
 
 const PictureSection = () => {
 
@@ -49,27 +49,29 @@ const PictureSection = () => {
             isSelected={tag === "Suzhou"}
           />
         </div>
-        <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12 mb-15">
-          {filteredPhotos.map((photo, index) => (
-            <motion.li
-              key={index}
-              variants={cardVariants}
-              initial="initial"
-              animate={isInView ? "animate" : "initial"}
-              transition={{ duration: 0.3, delay: index * 0.4 }}
-              className="border-1 border-[#999999]"
-            >
-              <h5 className="text-black text-xl font-bold underline underline-offset-2 py-2 px-3 mt-2 mb-4">{photo.title}</h5>
-              <PhotoCards
-                key={photo.id}
-                title={photo.title}
-                description={photo.description !== "" ? photo.description : "This guy is too lazy to leave a description for this photo."}
-                imgUrl={photo.image}
-                gitUrl={photo.gitUrl}
-              />
-              <div className="mb-5"></div>
-            </motion.li>
-          ))}
+        <ul key={tag} ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12 mb-15">
+          <AnimatePresence>
+            {filteredPhotos.map((photo, index) => (
+              <motion.li
+                key={index}
+                variants={cardVariants}
+                initial="initial"
+                animate={isInView ? "animate" : "initial"}
+                transition={{ duration: 0.3, delay: index * 0.4 }}
+                className="border-1 border-[#999999]"
+              >
+                <h5 className="text-black text-xl font-bold underline underline-offset-2 py-2 px-3 mt-2 mb-4">{photo.title}</h5>
+                <PhotoCards
+                  key={photo.id}
+                  title={photo.title}
+                  description={photo.description !== "" ? photo.description : "This guy is too lazy to leave a description for this photo."}
+                  imgUrl={photo.image}
+                  gitUrl={photo.gitUrl}
+                />
+                <div className="mb-5"></div>
+              </motion.li>
+            ))}
+          </AnimatePresence>
         </ul>
         <div className="w-full h-px bg-black mt-15" />
         <div className="w-full h-px bg-black mt-1 mb-8" />
