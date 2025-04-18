004190f2e289e053869e033e835b4914253f7d09
Jacky72 <initialstarry2002@gmail.com>
2025-04-18 23:10:26 +0800

    Finish the basic version of the video section

--- zhenqizhang_photogallery/src/app/components/VideoSection.jsx
+++ zhenqizhang_photogallery/src/app/components/VideoSection.jsx
@@ -33,34 +33,29 @@ const VideoSection = () => {
           />
           <Tags
             onClick={handleTagChange}
-            name="Beijing"
-            isSelected={tag === "Beijing"}
+            name="Fujian"
+            isSelected={tag === "Fujian"}
           />
           <Tags
             onClick={handleTagChange}
-            name="Suzhou"
-            isSelected={tag === "Suzhou"}
+            name="Xiamen"
+            isSelected={tag === "Xiamen"}
           />
         </div>
         <ul key={tag} className="grid md:grid-cols-3 gap-8 md:gap-12 mb-15">
           <AnimatePresence>
             {filteredVideos.map((video, index) => (
-              <Link key={video.id} href={`/video/`}> {/* ${video.location} */}
                 <motion.li
                   key={video.id}
-                  initial="initial"
-                  transition={{ duration: 0.3, delay: index * 0.4 }}
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -20 }}
+                  transition={{ duration: 0.3, delay: index * 0.1 }}
                 >
                   <h5 className="text-black text-xl font-bold text-center underline underline-offset-2 py-2 px-3 mt-2 mb-4">{video.title}</h5>
-                  <video className="w-90 h-auto object-cover lg:mx-auto p-1 border bg-white border-white shadow" src="/video/welcomevid.mp4" controls></video>
-                  {/* <video
-                    key={video.id}
-                    title={video.title}
-                    src={video.video}
-                  /> */}
+                  <video className="w-90 h-auto object-cover lg:mx-auto p-1 border bg-white border-white shadow" src={video.video} controls></video>
                   <div className="mb-5"></div>
                 </motion.li>
-              </Link>
             ))}
           </AnimatePresence>
         </ul>
--- zhenqizhang_photogallery/src/app/components/data/VideoData.jsx
+++ zhenqizhang_photogallery/src/app/components/data/VideoData.jsx
@@ -2,9 +2,37 @@ const videoData = [
     {
       id: 1,
       title: "Shanghai Vlog One",
-      video: "/video/welcomevid.mp4",
-      location: "",
+      video: "/video/Shanghai/Shanghai Vlog One.mp4",
+      location: "Shanghai",
       tag: ["All", "Shanghai"]
+    },
+    {
+      id: 2,
+      title: "Shanghai Vlog Two",
+      video: "/video/Shanghai/Shanghai Vlog Two.mp4",
+      location: "Shanghai",
+      tag: ["All", "Shanghai"]
+    },
+    {
+      id: 3,
+      title: "Fujian Vlog One",
+      video: "/video/Fujian/Fujian Vlog One.mp4",
+      location: "Fujian",
+      tag: ["All", "Fujian"]
+    },
+    {
+      id: 4,
+      title: "Fujian Vlog Two",
+      video: "/video/Fujian/Fujian Vlog Two.mp4",
+      location: "Fujian",
+      tag: ["All", "Fujian"]
+    },
+    {
+      id: 5,
+      title: "Xiamen Vlog One",
+      video: "/video/Xiamen/Xiamen Vlog One.mp4",
+      location: "Xiamen",
+      tag: ["All", "Xiamen"]
     },
 ];
   
